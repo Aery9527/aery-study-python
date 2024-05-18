2bc5b9baf191009853fe043b1b42ea4d78ef8577
Aery <nulllifeart@gmail.com>
2024-05-18 17:33:19 +0800

    study

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# aery-study-python
+- PEP8 (Python Enhancement Proposal 8), 是一種 Python 的程式碼風格指南
+- 變數全是reference, x = y = 10, x 和 y 都是指向同一個記憶體位置, 而不是像java原生型別一樣直接占用不同byte表達該數值
+- 社群變數命名習慣蛇行命名法(snake_case), 全為小寫字母用底線分隔(my_variable)
+- 動態強型別, 非弱型別. 弱型別可以執行"1" + 1, 但強型別不行, 可python宣告變數時不用指定型別, 於是是"動態強型別"
+- 支援functional programming
+- Python使用模組（module）和封裝（package）來組織代碼，類似於Java中的包和類。 \
+  但是，Python的模組系統更加靈活和簡單，並且模組可以在需要時動態加載。
+- Python的多線程在某些情況下可能不如預期效果好，這是因為全局解釋器鎖（Global Interpreter Lock，GIL）的存在。 \
+  因此，在Python中，更常見的是使用異步編程庫，如asyncio，來實現非阻塞的並發操作。
---
+++ study_base.py
@@ -0,0 +1,94 @@
+"""
+使用雙引號多行註解
+"""
+'''
+或者使用單引號多行註解
+'''
+
+RESULT = ">>>"
+
+# 計算符號
+print("10.0 / 2 =", 10 / 2, "# 結果是float")
+print("10 / 4 =", 10 / 4, "# 結果是float")
+print("10 // 4 =", 10 // 4, "# 商")
+print("10 ** 2 = " + str(10 ** 2) + " # 次方")  # 字串與數字不能直接相加, 要透過str()轉換
+
+# 雙引號(") 單引號(') 互相嵌套可以不需要逃逸字元 (這部分跟js一樣)
+print("kerker", "'9527'")
+print('kerker', '"9527"')
+
+# 同時指定
+a = b = c = 5
+print("a = b = c = 5", RESULT, a, b, c)
+a, b, c = 4, 5, 6
+print("a, b, c = 4, 5, 6", RESULT, a, b, c)
+
+# 交換
+a, b, c = b, c, a
+print("a, b, c = b, c, a", RESULT, a, b, c, "# 交換")
+
+# 多行接續, 使用繼續符號(\), 後面不可以接東西, 註解也不行
+a = 1 \
+    + 2 \
+    + 3
+b = (1  # 使用括弧接續就可以放註解在後面
+     + 2
+     + 3
+     )
+print("a", RESULT, a)
+print("b", RESULT, b)
+print("id(a)", RESULT, id(a), "# 記憶體位置")
+print("id(b)", RESULT, id(b), "# 記憶體位置")
+
+# 匯入模組
+import math
+
+print("math.pi", RESULT, math.pi)
+
+# 查型態, python3之後無int/long/float/double限制, 所以基本上就是無限大
+print("type(10)", RESULT, type(10))
+print("type(10.0)", RESULT, type(10.0))
+print("type('kerker')", RESULT, type('kerker'))
+print("type(True)", RESULT, type(True))
+print("type(False)", RESULT, type(False))
+print("type(None)", RESULT, type(None))
+
+# 進制轉換
+print("0b1010", RESULT, 0b1010, "# 二進制")
+print("0o12", RESULT, 0o12, "# 八進制")
+print("0xA", RESULT, 0x0A, "# 十六進制")
+print("bin(10)", RESULT, bin(10), type(bin(10)), "# 轉二進制")
+print("oct(10)", RESULT, oct(10), type(oct(10)), "# 轉八進制")
+print("hex(10)", RESULT, hex(10), type(hex(10)), "# 轉十六進制")
+print("int('1010', 2)", RESULT, int('1010', 2), "# 二進制轉十進制")
+print("int('12', 8)", RESULT, int('12', 8), "# 八進制轉十進制")
+print("int('A', 16)", RESULT, int('A', 16), "# 十六進制轉十進制")
+print("int(10.5)", RESULT, int(10.5), "# 轉整數")
+print("float(10)", RESULT, float(10), "# 轉浮點數")
+
+# 常用數學方法
+print("abs(-10)", RESULT, abs(-10), "# 絕對值")
+print("pow(10, 2)", RESULT, pow(10, 2), "# 次方")  # 同 ** 計算子
+print("round(10.5)", RESULT, round(10.5), "# 四捨五+1入")  # Bankers' Rounding
+print("round(10.51)", RESULT, round(10.51), "# 四捨五+1入")
+print("round(1.055, 2)", RESULT, round(1.055, 2), "# 四捨五+1入")
+print("round(1.0551, 2)", RESULT, round(1.0551, 2), "# 四捨五+1入")
+
+# 科學記號, 一定是float
+print("12345e-2", RESULT, 12345e-2)
+print("123.45e2", RESULT, 123.45e2)
+
+# boolean, 0或空資料為False, 其餘皆為True
+print("bool(0.0)", RESULT, bool(0.0))
+print("bool(0)", RESULT, bool(0))
+print("bool(None)", RESULT, bool(None))
+print("bool('')", RESULT, bool(''), "# 空字串")
+print("bool([])", RESULT, bool([]), "# 空串列")
+print("bool(())", RESULT, bool(()), "# 空元組")
+print("bool({})", RESULT, bool({}), "# 空字典")
+print("bool(set())", RESULT, bool(set()), "# 空集合")
+print("bool(-0.1)", RESULT, bool(0.1))
+print("bool(0.1)", RESULT, bool(0.1))
+print("bool(1)", RESULT, bool(1))
+print("int(True)", RESULT, int(True))
+print("int(False)", RESULT, int(False))
