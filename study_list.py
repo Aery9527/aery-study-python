4707b2123248bc652cc3fdf22c90b1cfe5b538af
Aery <nulllifeart@gmail.com>
2024-07-09 07:41:19 +0800

    study list

--- study_list.py
+++ study_list.py
@@ -43,6 +43,32 @@ list2[0] = 9
 print(showFormat.format("list1", RESULT, list1))
 print(showFormat.format("list2", RESULT, list2))
 
+print(TITLE.format("max, min, sum"))
+listNumber = [2, 4, 1, 6, 7, 5, 0, 3, 9, 8]
+listWord = ['J', 'H', 'A', 'G', 'B', 'C', 'E', 'D', 'I', 'F']  # sort by Unicode, can't use sum
+listDiff = [0, 1, 'G', 'F', 4, 'H', 2, 'J', 'I', 3]  # can't use max, min, sum
+print(showFormat.format("max(listNumber)", RESULT, max(listNumber)))
+print(showFormat.format("min(listNumber)", RESULT, min(listNumber)))
+print(showFormat.format("sum(listNumber)", RESULT, sum(listNumber)))
+print(showFormat.format("max(listWord)", RESULT, max(listWord)))
+print(showFormat.format("min(listWord)", RESULT, min(listWord)))
+
+print(TITLE.format("del list[start:stop:step]"))
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+print(showFormat.format("list", RESULT, list))
+del list[0]
+print(showFormat.format("del list[0]", RESULT, list))
+del list[0:2]
+print(showFormat.format("del list[0:2]", RESULT, list))
+del list[0::2]
+print(showFormat.format("del list[0::2]", RESULT, list))
+
+print(TITLE.format("多重指定與打包變數"))
+a, b, *c = 1, 2, 3, 4, 5
+print(showFormat.format("a, b, *c = 1, 2, 3, 4, 5", RESULT, "{0} {1} {2}".format(a, b, c)))
+a, *b, c = 1, 2, 3, 4, 5
+print(showFormat.format("a, *b, c = 1, 2, 3, 4, 5", RESULT, "{0} {1} {2}".format(a, b, c)))
+
 print(TITLE.format("list 包含的方法"))
 list1Dir = dir(list)
 public_attributes = [attr for attr in list1Dir if not attr.startswith('_')]
